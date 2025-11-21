8a2e353c2fb40fe9005922a550567c0208baff46
Perezcfigueira <perezcfigueira@gmail.com>
2025-11-20 23:31:43 -0400

    fix problems with list

--- src/components/ListaDeTareas.jsx
+++ src/components/ListaDeTareas.jsx
@@ -1,41 +1,56 @@
 import { useState } from "react";
 
 export const Tareas = () => {
+  const [tareas, setTareas] = useState(["comprar leche"]);
+  const [nuevaTarea, setNuevaTarea] = useState("");
 
-    const [tareas, setTareas] = useState(["comprar leche"])
-    const [nuevaTarea, setNuevaTarea] = useState("")
+  return (
+    <>
+    <div class="input-group flex-nowrap">
+      <span class="input-group-text" id="addon-wrapping">
+        @
+      </span>
 
+      <input
+        value={nuevaTarea}
+        onChange={(event) => {
+          setNuevaTarea(event.target.value);
+        }}
+        onKeyDown={(event)=>{
+            if (event.key==="Enter"){
+                setTareas((prev) => {
+            return [...prev, nuevaTarea];
+          });
+          setNuevaTarea(" ")
+            }
+        }}
+        type="text"
+        class="form-control"
+        placeholder="Username"
+        aria-label="Username"
+        aria-describedby="addon-wrapping"
+      />
+      <button
+        onClick={() => {
+          setTareas((prev) => {
+            return [...prev, nuevaTarea];
+          });
+          setNuevaTarea(" ")
+        }}
+        
+        type="button"
+        class="btn btn-primary"
+      >
+        agregar
+      </button>
+
+    </div>
     
-
-
-    return(
-
-        <div class="input-group flex-nowrap">
-        <span class="input-group-text" id="addon-wrapping">@</span>
-
-         value={nuevaTarea}
-        <input onChange={(event) => {
-
-            setNuevaTarea(event.target.value)
-        }}
-        type="text" 
-        class="form-control" 
-        placeholder="Username" 
-        aria-label="Username" 
-        aria-describedby="addon-wrapping"/>
-        <button onClick={() =>{
-            setTareas([nuevaTarea])
-            setNuevaTarea(' ')
-
-        }}
-        type="button" 
-        class="btn btn-primary">agregar</button>
-
-        <ul>
-            {tareas.map((tareas, index) => 
-            <li key={index}>{tareas}</li>
-            )}
-        </ul>
-        </div>
-    )
-}
+      <ul>
+        {tareas.map((tareas, index) => (
+          <li key={index}>{tareas}</li>
+        ))}
+      </ul>
+      </>
+  );
+};
